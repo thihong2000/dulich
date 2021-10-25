5fa8dd73f177eaef1a81c7387c35a36e472df2cd
thihong2000 <69857496+thihong2000@users.noreply.github.com>
2021-10-25 13:11:40 +0700

    upbai

---
+++ tms/tms/tests/Unit/LoginTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class LoginTest extends \PHPUnit\Framework\TestCase
+{
+	public function testViewIndexLogin(){
+    	// $response = $this->get('/dang-nhap');
+     //    $response->assertStatus(200);
+        $this->assertEquals('a','a');
+    }
+}

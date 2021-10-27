b3ba9571f71856319837fb712a9926305aba89ac
Van Nhat Thao <nhatthaogander@gmail.com>
2021-10-27 14:14:51 +0700

    bỏ sung

--- tms/tms/package-details.php
+++ tms/tms/package-details.php
@@ -83,8 +83,34 @@ $error="Có một vài thứ bị lỗi, Vui lòng thử lại";
 <!-- top-header -->
 <?php include('includes/header.php');?>
 <div class="banner-3">
+<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+	
+		
+				
+					<nav class="cl-effect-1">
+						<ul class="nav navbar-nav">
+							
+			
+	
+							<li><a href="index.php">Trang chủ</a></li>
+							<li><a href="page.php?type=Giới thiệu">Về chúng tôi</a></li>
+								<li><a href="package-list.php">Các Gói Du Lịch</a></li>
+								<li><a href="page.php?type=Chính sách bảo mật">Chính sách bảo mật</a></li>
+								<li><a href="page.php?type=Điều kiện và điều khoản">Điều kiện và điều khoản</a></a></li>
+								<li><a href="page.php?type=Liên hệ">Liên hệ</a></li>
+								<?php if($_SESSION['login'])
+{?>
+								<li>Bạn cần hỗ trợ?<a href="#" data-toggle="modal" data-target="#myModal3"> / Hãy cho chúng tôi biết </a>  </li>
+								<?php } else { ?>
+								<li><a href="enquiry.php">Xem xét </a>  </li>
+								<?php } ?>
+								<div class="clearfix"></div>
+
+						</ul>
+					</nav>
+				</div><!-- /.navbar-collapse -->	
 	<div class="container">
-		<h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;"> N7 - Chi tiết gói</h1>
+		<h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">Chi tiết</h1>
 	</div>
 </div>
 <!--- /banner ---->

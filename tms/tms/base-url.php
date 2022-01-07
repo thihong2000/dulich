b4329a30bc9b66875e2053b143cf128a3d99b8c3
Van Nhat Thao <56481766+nhatthaovan@users.noreply.github.com>
2022-01-08 01:18:56 +0700

    update base-url

--- tms/tms/base-url.php
+++ tms/tms/base-url.php
@@ -1,6 +1,16 @@
 <?php
 	function base_url()
     {      
+        // chỗ 88 nhớ sửa theo dịa chỉ localhost của máy nha
         return $url  = "http://localhost:88/tms/"; 
+    }
+    function arrayValueDB($sql)
+    {   
+    	include('includes/config.php');
+        $query = $dbh -> prepare($sql);
+		$query->execute();
+		$results=$query->fetchAll(PDO::FETCH_OBJ);
+		$cnt=$query->rowCount();
+		return $cnt;
     }
 ?>
